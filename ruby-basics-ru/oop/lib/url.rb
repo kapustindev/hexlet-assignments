5ad065ea7fad1d755b88b032b4db582a0e64f2b6
@hexlet/cli <support@hexlet.io>
2022-11-20 18:25:28 +0400

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -1,30 +1,34 @@
 # frozen_string_literal: true
 
 # BEGIN
+require 'forwardable'
+
 class Url
-  attr_reader :scheme, :url, :host
+  attr_reader :url
+  extend Forwardable
+  def_delegators :url, :scheme, :host
+
   include Comparable
   def ==(other)
-    url == other.url
+    url.to_s == other.url.to_s
   end
 
   def initialize(url)
-    @url = url
-    @scheme = URI(url).scheme
-    @host = URI(url).host
+    @url = URI(url)
   end
 
   def query_params
-    query = URI(url).query
+    query = url.query
     return {} if query.nil?
+  
     query.split('&').each_with_object({}) do |q, acc|
       key, value = q.split('=')
       acc[key.to_sym] = value
     end
   end
 
-  def query_param(key, defaultValue = nil)
-    query_params[key] ||= defaultValue
+  def query_param(key, default_value = nil)
+    query_params[key] ||= default_value
   end
 end
 # END
