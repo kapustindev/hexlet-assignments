23f8f40b50f5699b6954b8407edb64460d9eaddf
@hexlet/cli <support@hexlet.io>
2023-01-17 11:19:26 +0400

    submit rails-basics-ru/relations

---
+++ rails-basics-ru/relations/app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find params[:id]
+  end
+
+  def new
+    @task = Task.new
+    @users = User.all
+    @statuses = Status.all
+  end
+
+  def edit
+    @task = Task.find params[:id]
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: 'Task was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @task = Task.find params[:id]
+
+    if @task.update(task_params)
+      redirect_to @task, notice: 'Task was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find params[:id]
+
+    @task.destroy
+
+    redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :status_id, :user_id)
+  end
+end
