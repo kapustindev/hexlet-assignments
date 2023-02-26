f08fa15b96469487cb27ae0fe281dc6533fecbaa
@hexlet/cli <support@hexlet.io>
2023-02-26 22:49:52 +0400

    submit rails-real-ru/search-forms

---
+++ rails-real-ru/search-forms/app/models/post.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Post < ApplicationRecord
+  include AASM
+
+  validates :title, presence: true
+  validates :body, length: { maximum: 500 }
+
+  aasm do
+    state :draft, initial: true
+    state :published
+
+    event :publish do
+      transitions from: :draft, to: :published
+    end
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["aasm_state", "title"]
+  end
+end
